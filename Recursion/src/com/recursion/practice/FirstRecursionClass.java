e5a1d5504ba837a70dbf7b189b97ae1b24fb529a
tusharsaran <tusharsaran@yahoo.com>
2020-06-04 22:05:45 -0700

    first draft commit of recursion project

---
+++ Recursion/src/com/recursion/practice/FirstRecursionClass.java
@@ -0,0 +1,23 @@
+package com.recursion.practice;
+/*
+ * this is a first simple recursion class
+ */
+
+public class FirstRecursionClass {
+
+	public static void main(String[] args) {
+		showNumber(5);
+	}
+	
+	static int showNumber(int num) {
+		if(num <  1) {
+			System.out.println("the number is  : " +  num);
+			return num;
+		}else {
+			showNumber(num -  1);
+			System.out.println("the number is  : " +  num);
+		}
+		return 0;
+	}
+
+}
