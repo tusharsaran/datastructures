0576ed3e464bb83a5f88e92ea0e22b69960d6d60
tusharsaran <tusharsaran@yahoo.com>
2020-06-07 13:10:19 -0700

    added a new code for NameReversal

---
+++ Recursion/src/com/recursion/practice/basics/NameReversal.java
@@ -0,0 +1,22 @@
+package com.recursion.practice.basics;
+
+public class NameReversal {
+
+	public static void main(String[] args) {
+		System.out.println(reverseNameRecursive("Aarunya"));
+	}
+	
+	static String reverseNameRecursive(String theName){
+		if(theName.length() == 0) {
+			return theName;
+		}
+		//System.out.println(theName.substring(1));
+		//System.out.println(theName.charAt(0));
+		System.out.println("aa :::" + theName.substring(1) + theName.charAt(0));
+		return (reverseNameRecursive(theName.substring(1)) + theName.charAt(0)) ;
+	}
+
+	/*A A R U N Y A
+	A Y N U	R A	A*/
+	
+}
