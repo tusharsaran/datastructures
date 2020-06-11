cbcc1ba45423c69ca6be596ca5dfaed9654a2850
tusharsaran <tusharsaran@yahoo.com>
2020-06-10 20:51:54 -0700

    added few programs on recursive

---
+++ Recursion/src/com/recursion/practice/basics/BiggestNumberInArray.java
@@ -0,0 +1,26 @@
+package com.recursion.practice.basics;
+
+public class BiggestNumberInArray {
+
+	public static void main(String[] args) {
+		int [] numbers = {3,2,30,5, 50,60,12,54,32,86,57,12,6,9};
+		System.out.println(findBiggestNumber(numbers, numbers.length-1));
+
+	}
+	
+	static int findBiggestNumber(int [] numArary, int length) {		
+		if(length == 0) {
+			return numArary[length];
+		}else {		
+			int output =  findBiggestNumber(numArary , length - 1);
+			//System.out.println("output is " + output);
+			int num = numArary[length];
+			//System.out.println("num is " + num);
+			int highestNumber =  output > num ? output : num;
+			//System.out.println("highestNumber is " + highestNumber);
+			return highestNumber;
+			}
+		
+	}
+
+}
--- Recursion/src/com/recursion/practice/basics/NameReversal.java
+++ Recursion/src/com/recursion/practice/basics/NameReversal.java
@@ -3,17 +3,23 @@ package com.recursion.practice.basics;
 public class NameReversal {
 
 	public static void main(String[] args) {
-		System.out.println(reverseNameRecursive("Aarunya"));
+		System.out.println(reverseNameRecursive("Pari"));
 	}
 	
 	private static String reverseNameRecursive(String theName){
+		String name = null;
 		if(theName.length() == 0) {
 			return theName;
 		}
 		//System.out.println(theName.substring(1));
-		//System.out.println(theName.charAt(0));
-		System.out.println("aa :::" + theName.substring(1) + theName.charAt(0));
-		return (reverseNameRecursive(theName.substring(1)) + theName.charAt(0)) ;
+		//System.out.println("charAt value is : " + theName.charAt(0));
+		//System.out.println("sub string  value is : " +theName.substring(1));
+		//System.out.println("lenght is : " +theName.substring(1).length());
+		 name =  reverseNameRecursive(theName.substring(1));
+		 //System.out.println("name is " + name);
+		 String output =  name + theName.charAt(0);
+		 
+		 return output;
 	}
 
 	/*A A R U N Y A
