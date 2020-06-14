5f0827a16304ee427ed9018b056fdc01cb5d91fc
tusharsaran <tusharsaran@yahoo.com>
2020-06-13 23:11:38 -0700

    added code in array

---
+++ Array/src/com/array/practice/sngleDimensionArray/SearchingArray.java
@@ -0,0 +1,46 @@
+package com.array.practice.sngleDimensionArray;
+
+public class SearchingArray {
+
+	public static void main(String[] args) {
+		int[] arry = {2,445,566,23,353};
+		findArrayIfNumberExists(arry, 2);
+		findCellNumberIfNumberExists(arry, 232);
+
+	}
+	
+	public static void findArrayIfNumberExists(int [] arry, int noToSearch) {
+		boolean isexists = false;
+		try {
+			for(int i = 0; i <= arry.length - 1; i++) {
+				if(arry[i] == noToSearch) 
+					isexists = true;
+			}	
+			if(isexists) {
+				System.out.println(noToSearch + " exists in array");
+				}else {
+					System.out.println("number  does not exist in array");
+				}
+		}catch(Exception ex) {
+			System.out.println("Array do not exists.");
+		}
+	}
+	
+	public static void findCellNumberIfNumberExists(int [] arry, int noToSearch) {
+		int cellNo = 0;
+		try {
+			for(int i = 0; i <= arry.length - 1; i++) {
+				if(arry[i] == noToSearch) 
+					cellNo = i;
+			}	
+			if(cellNo > 0) {
+				System.out.println("cell No is "+ cellNo);
+				}else {
+					System.out.println("number does not exist in array");
+				}
+		}catch(Exception ex) {
+			System.out.println("Array do not exists.");
+		}
+	}
+
+}
---
+++ Array/src/com/array/practice/sngleDimensionArray/TraversingArray.java
@@ -0,0 +1,22 @@
+package com.array.practice.sngleDimensionArray;
+
+public class TraversingArray {
+
+	public static void main(String[] args) {
+		int[] arry = {2,445,566,23,353};
+		traverseArray(arry);
+
+	}
+	
+	public static void traverseArray(int [] arry) {
+		try {
+			for(int i = 0; i <= arry.length - 1; i++) {
+				System.out.println("arry value is  :" + arry[i]);
+				
+			}
+		}catch(Exception ex) {
+			System.out.println("Array do not exists.");
+		}
+	}
+
+}
