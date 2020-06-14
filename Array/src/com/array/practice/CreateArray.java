037b0a8eff01f517a8a57eb9acbfcb7223fb18ef
tusharsaran <tusharsaran@yahoo.com>
2020-06-13 21:01:30 -0700

    added code for creating an array

---
+++ Array/src/com/array/practice/CreateArray.java
@@ -0,0 +1,27 @@
+package com.array.practice;
+/*
+ * Time complexity :
+ * T(ß∞) =  O(n)
+ */
+public class CreateArray {
+
+	public static void main(String[] args) {
+		//declaring an array // ------------------ O(1)
+		int[] arry;   
+		
+		//creating an array //-------------- O(1)
+		arry = new int[5]; 
+		
+		//initializing an array // --------------------- O(n)
+		arry[0] = 3; 
+		arry[1] = 6;
+		arry[2] = 0;
+		arry[3] = 534;
+		arry[4] = 2;
+		
+		System.out.println(arry[4]);
+			
+
+	}
+
+}
---
+++ Array/src/com/array/practice/CreateArraySimpleway.java
@@ -0,0 +1,15 @@
+package com.array.practice;
+/*
+ * Time complexity :
+ * T(∞) =  O(1) + 01
+ * 		=> O(1)
+ */
+public class CreateArraySimpleway {
+
+	public static void main(String[] args) {
+		//Declaration, Instantiation and initialization ---  T(n) = O(1) as we do not loop while creating an array 
+		int[] arry = {2,445,566,23,353}; //--------------------------O(1)
+		System.out.println(arry[4]); //------------------------------O(1)
+	}
+
+}
